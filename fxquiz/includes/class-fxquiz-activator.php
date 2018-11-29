1ccb3ffd377d8ae7ae90b5101c9178dc4810c735
shriyafxbytes <jain.shriya@fxbytes.com>
2018-11-29 16:36:58 +0530

    plugin initializing with boilerplate

---
+++ fxquiz/includes/class-fxquiz-activator.php
@@ -0,0 +1,37 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       #
+ * @since      1.0.0
+ *
+ * @package    Fxquiz
+ * @subpackage Fxquiz/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Fxquiz
+ * @subpackage Fxquiz/includes
+ * @author     Shriya Jain <#>
+ */
+class Fxquiz_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+
+	}
+
+}
